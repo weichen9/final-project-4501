8951e63805863c66a20e219caf30bfcd26da9711
Honsan96 <zh2444@columbia.edu>
2022-05-01 17:19:22 -0400

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,48 +1,45 @@
-CREATE TABLE IF NOT EXISTS yellow
-(
-    id INTEGER PRIMARY KEY,
-    pickup_datetime DATETIME,
-    dropoff_datetime DATETIME,
-    pickup_longitude float,
-    pickup_latitude float,
-    dropoff_longitude float,
-    dropoff_latitude float,
-    tip_amount float,
-    total_amount float,
-    distance float  
-    
-);
 
-CREATE TABLE IF NOT EXISTS uber
-(
-    id INTEGER PRIMARY KEY,
-    key DATETIME,
-    fare_amount float,
-    pickup_longitude float,
-    pickup_latitude float,
-    dropoff_longitude float,
-    dropoff_latitude float,
-    passenger_count,
-    distance float  
-    
-);
-
-CREATE TABLE IF NOT EXISTS daily_w
-(
-    id INTEGER PRIMARY KEY,
-    DATE DATETIME,
-    dailywindspeed float,
-    dailyprecipitation float
-    
-    
-);
-
-CREATE TABLE IF NOT EXISTS hourly_w
-(
-    id INTEGER PRIMARY KEY,
-    DATE DATETIME,
-    HourlyWindSpeed float,
-    HourlyPrecipitation float
-    
-    
+CREATE TABLE taxi_data (
+	id INTEGER NOT NULL, 
+	pickup_datetime DATETIME, 
+	dropoff_datetime DATETIME, 
+	pickup_longitude FLOAT, 
+	pickup_latitude FLOAT, 
+	dropoff_longitude FLOAT, 
+	dropoff_latitude FLOAT, 
+	tip_amount FLOAT, 
+	total_amount FLOAT, 
+	distance FLOAT, 
+	PRIMARY KEY (id)
 )
+
+
+CREATE TABLE daily_weather (
+	id INTEGER NOT NULL, 
+	"Date" DATE, 
+	"DailyWindSpeed" FLOAT, 
+	"DailyPrecipitation" FLOAT, 
+	PRIMARY KEY (id)
+)
+
+
+CREATE TABLE hourly_weather (
+	id INTEGER NOT NULL, 
+	"Date" DATETIME, 
+	"HourlyWindSpeed" FLOAT, 
+	"HourlyPrecipitation" FLOAT, 
+	PRIMARY KEY (id)
+)
+
+
+CREATE TABLE uber (
+	id INTEGER NOT NULL, 
+	pickup_datetime DATETIME, 
+	pickup_longitude FLOAT, 
+	pickup_latitude FLOAT, 
+	dropoff_longitude FLOAT, 
+	dropoff_latitude FLOAT, 
+	distance FLOAT, 
+	PRIMARY KEY (id)
+)
+
